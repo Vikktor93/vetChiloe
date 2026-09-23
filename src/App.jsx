a8bbd5adeb61455dcf95d46f17c0ddb915b87fa9
LizzIna <valentinacalderon466@gmail.com>
2026-09-23 12:02:15 -0300

    Cambio de Nombre de pacientes y Veterinaria

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import TarjetaPaciente from './components/TarjetaPaciente'; // se importa el com
 
 function App() {
   const [pacientes, setPacientes] = useState([
-    { id: 1, nombre: 'Charkicito', numero_atencion: '2026-A1' },
-    { id: 2, nombre: 'Mercedes', numero_atencion: '2026-B2' },
-    { id: 3, nombre: 'Hannita', numero_atencion: '2026-C3' }
+    { id: 1, nombre: 'Poi', numero_atencion: '2026-A1' },
+    { id: 2, nombre: 'Abeguin', numero_atencion: '2026-B2' },
+    { id: 3, nombre: 'Raneguin', numero_atencion: '2026-C3' }
   ]);
 
   return (
     <div className="contenedor-principal">
       <header className="cabecera">
-        <h1>Sistema de Información - Veterinaria Chiloé</h1>
+        <h1>Sistema de Información - VetPenguin</h1>
         <p>Plataforma de control y seguimiento de pacientes</p>
       </header>
 
